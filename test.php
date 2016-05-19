60cbf386b51ca1b62bcc3a0ed7258399d80b9334
jcheney@free.fr <cheney01>
2016-05-19 15:18:50 +0200

    a

--- test.php
+++ test.php
@@ -1,12 +1,11 @@
 <?php
-echo "1";
 use Att\M2X\M2X;
 use Att\M2X\Error\M2XException;
-echo "2";
+
 //$apiKey   = getenv("M2X_API_KEY");
 $apiKey = "aa554d2899305c234358af041ba9366b";
 $deviceId = 'fe1ee628c3549e9de86d7101d9c5317f';
-echo "3";
+
 $m2x = new M2X($apiKey);
 
 # Get the device
@@ -26,9 +25,9 @@ $device->updateStream('load_15m');
     'load_5m'  => array(array('value' => $load_5m,  'timestamp' => $now)),
     'load_15m' => array(array('value' => $load_15m, 'timestamp' => $now))
   );
-  echo "load_1m.value=".$values['load_1m'][0]['value']." load_1m.timestamp=".$values['load_1m'][0]['timestamp']."\n";  
-  echo "load_5m.value=".$values['load_5m'][0]['value']." load_5m.timestamp=".$values['load_5m'][0]['timestamp']."\n";
-  echo "load_15m.value=".$values['load_15m'][0]['value']." load_15m.timestamp=".$values['load_15m'][0]['timestamp']."\n";
+  echo "load_1m.value=".$values['load_1m'][0]['value']." load_1m.timestamp=".$values['load_1m'][0]['timestamp']."\n<br/>";  
+  echo "load_5m.value=".$values['load_5m'][0]['value']." load_5m.timestamp=".$values['load_5m'][0]['timestamp']."\n<br/>";
+  echo "load_15m.value=".$values['load_15m'][0]['value']." load_15m.timestamp=".$values['load_15m'][0]['timestamp']."\n<br/>";
 
   try {
     $device->postUpdates($values);
--- test1.php
+++ test1.php
@@ -9,8 +9,8 @@
     'load_15m' => array(array('value' => $load_15m, 'timestamp' => $now))
   );
   
-  echo "load_1m.value=".$values['load_1m'][0]['value']." load_1m.timestamp=".$values['load_1m'][0]['timestamp']."\n";  
-  echo "load_5m.value=".$values['load_5m'][0]['value']." load_5m.timestamp=".$values['load_5m'][0]['timestamp']."\n";
-  echo "load_15m.value=".$values['load_15m'][0]['value']." load_15m.timestamp=".$values['load_15m'][0]['timestamp']."\n";
+  echo "load_1m.value=".$values['load_1m'][0]['value']." load_1m.timestamp=".$values['load_1m'][0]['timestamp']."\n<br/>";  
+  echo "load_5m.value=".$values['load_5m'][0]['value']." load_5m.timestamp=".$values['load_5m'][0]['timestamp']."\n<br/>";
+  echo "load_15m.value=".$values['load_15m'][0]['value']." load_15m.timestamp=".$values['load_15m'][0]['timestamp']."\n<br/>";
 
 ?>

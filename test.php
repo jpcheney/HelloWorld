aa81c72af622d178ff72b22c1a56ea5f6bd2d168
jcheney@free.fr <cheney01>
2016-05-19 16:26:51 +0200

    a

--- test.php
+++ test.php
@@ -5,12 +5,13 @@ use Att\M2X\M2X;
 use Att\M2X\Error\M2XException;
 */
 echo "getenv(\"M2X_API_KEY\")=".getenv("M2X_API_KEY")."<br/>";
-//echo "getenv(\"M2X_URL\")=".getenv("M2X_URL")."<br/>";
+echo "getenv(\"M2X_URL\")=".getenv("M2X_URL")."<br/>";
 $apiKey = getenv("M2X_API_KEY");
 //$apiKey = "587f7006234b4059d15de39c98dd7a4a";
 $deviceId = '434b72300bad423cbaa295f7db30404c';
 echo "test1<br/>";
-/*
+
+
 $m2x = new M2X($apiKey);
 echo "test1<br/>";
 # Get the device

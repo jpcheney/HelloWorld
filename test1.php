e6b04461db8e17459b006f0f4ccc947774fc0dd6
jcheney@free.fr <cheney01>
2016-05-19 15:11:20 +0200

    a

--- test1.php
+++ test1.php
@@ -8,10 +8,9 @@
     'load_5m'  => array(array('value' => $load_5m,  'timestamp' => $now)),
     'load_15m' => array(array('value' => $load_15m, 'timestamp' => $now))
   );
-  /*
-  echo "load_1m."values['load_1m']."\n"
-  echo "load_5m."values['load_5m']."\n"
-  echo "load_15m."values['load_15m']."\n"
-*/
-var_dump($values);
+  
+  echo "load_1m.value".$values['load_1m']['value']." load_1m.timestamp".$values['load_1m']['timestamp']."\n"
+  echo "load_5m.value".$values['load_5m']['value']." load_5m.timestamp".$values['load_5m']['timestamp']."\n"
+  echo "load_15m.value".$values['load_15m']['value']." load_15m.timestamp".$values['load_15m']['timestamp']."\n"
+
 ?>

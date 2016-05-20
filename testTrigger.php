41aa96ded631cef68c43420165e562fc96791d3e
jcheney@free.fr <cheney01>
2016-05-20 12:41:13 +0200

    a

--- testTrigger.php
+++ testTrigger.php
@@ -1,16 +1,16 @@
-Test
+Test<br/>
 
 <?php
-	$values = "method=". $_SERVER['REQUEST_METHOD']."<br/>GET<br/>";
+	$values = "method=". $_SERVER['REQUEST_METHOD']."<br/>GET<br/>\n";
 	if(count($_GET)>0){
 		foreach($_GET as $key => $getValues){
-			$values = $values . $key ."=" . $getValues . "<br/>";
+			$values = $values . $key ."=" . $getValues . "<br/>\n";
 		}
 	}
-	$values = $values . "POST<br/>";
+	$values = $values . "POST<br/>\n";
 	if(count($_POST)>0){
 		foreach($_POST as $key => $getValues){
-			$values = $values . $key ."=" . $getValues . "<br/>";
+			$values = $values . $key ."=" . $getValues . "<br/>\n";
 		}
 	}
 	$retour = file_put_contents("test.txt",$values,FILE_APPEND);

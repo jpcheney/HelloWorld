2d102258fb664edde8cd70a12e2c1c6bec0dd3a4
jcheney@free.fr <cheney01>
2016-05-20 11:10:32 +0200

    a

--- testTrigger.php
+++ testTrigger.php
@@ -1,7 +1,16 @@
 Test
 
 <?php
-	$retour = file_put_contents("test.txt","test");
+	$values = "GET<br/>";
+	foreach($_GET as $ket => $getValues){
+		$values = $values . $key ."=" . $getValues . "<br/>";
+	}
+	$values = $values . "POST<br/>";
+	foreach($_POST as $ket => $getValues){
+		$values = $values . $key ."=" . $getValues . "<br/>";
+	}
+	
+	$retour = file_put_contents("test.txt",);
 	echo $retour."</BR>";
 	
 	echo file_get_contents("test.txt");
